d8ea7e45da7ae65583709c0329079d56e29ba380
Baylee Quisenberry <bayquiz@me.com>
2022-12-14 09:11:13 -0500

    Update streamlit_app.py
    fixed index

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,9 @@ streamlit.header("🍎🍊 Build Your Own Fruit Smoothie 🍇🍉")
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit)
+
+#add multi select
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+#display table
 streamlit.dataframe(my_fruit_list)
